cd9bff071cd65d9329d4d0d696d1079157326099
knightofknee <bricarlis@gmail.com>
2017-07-19 11:56:41 -0500

    demo upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var cool =  require('cool-ascii-faces')
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +13,10 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/cool', function(request, response) {
+  response.send(cool())
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
